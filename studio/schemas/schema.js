f2755cef7e13312d7277a5bb41c955b0f0d10b1c
sharan shekar <50232498+sharan7blue@users.noreply.github.com>
2020-05-15 15:57:38 +0100

    Update schema.js

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -58,7 +58,7 @@ import blogAuthorReference from './objects/blogAuthorReference'
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'blog',
+  name: 'Liberty-v1',
   // Then proceed to concatenate our our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
